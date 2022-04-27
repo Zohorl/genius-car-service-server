dda4c0ecf83b46f0bd18aa16c3395bd874780de4
Zohorl <zohorul109561@gmail.com>
2022-04-27 22:08:25 +0600

    adding steps

---
+++ __server__deploy.js
@@ -0,0 +1,16 @@
+/**
+ * --------------------
+ * ONE TIME for computer
+ * ---------------------
+ * 1.create heroku account
+ * 2.verify email
+ * 3.install heroku cli
+ * 4.heroku login
+ * 
+ * ----------------------------
+ * ONE TIME for each project
+ * ----------------------------
+ * 1.heroku create
+ * 2.make sure you : git add. , git commit , git push
+ * 3.git push heroku main
+*/
