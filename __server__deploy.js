4e9f288ded650845ae14da4089f39579d7c095ef
Zohorl <zohorul109561@gmail.com>
2022-04-27 22:32:52 +0600

    added api

--- __server__deploy.js
+++ __server__deploy.js
@@ -13,4 +13,15 @@
  * 1.heroku create
  * 2.make sure you : git add. , git commit , git push
  * 3.git push heroku main
+ * 4.go to heroku dashboard>current project >settings>reveal config vars>
+ * 5.copy paste config vars from .env file
+ * 6.make sure you have whitelisted all ip address to access mongodb
+ * 
+ * ----------------------------------
+ * UPDATE SERVER with new changes
+ * ----------------------------------
+ * 1.make changes
+ * 2.make sure you : git add. , git commit , git push
+ * 3.git push heroku main
+ * 
 */
--- index.js
+++ index.js
@@ -109,6 +109,10 @@ app.get('/', (req, res) => {
     res.send('Running genius server');
 });
 
+app.get('/hero', (req, res) => {
+    res.send('hero meets heroku!!!')
+})
+
 app.listen(port, () => {
     console.log('listening to port', port);
 });
